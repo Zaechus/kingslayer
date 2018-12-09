69c989aa59d9854f8c9cc8a49bdc784072059a01
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2018-12-09 15:31:14 -0700

    try doc generation again

---
+++ site2docs.py
@@ -0,0 +1,20 @@
+import os, re, shutil
+from subprocess import call
+
+shutil.rmtree('docs', ignore_errors=True)
+
+os.chdir("site")
+call(["npm", "run", "build"])
+os.chdir("..")
+
+shutil.copytree("site/dist", "docs")
+shutil.rmtree("site/dist")
+
+data = ''
+with open("docs/index.html", "r") as index:
+    data = index.read().replace('\n', '')
+    data = re.sub(r'\bhref=/\b', 'href=', data)
+    data = re.sub(r'\bsrc=/\b', 'src=', data)
+
+with open("docs/index.html", "w") as index:
+    index.write(data)
