70291ec0e33a63c753737ffcea0bc21ef8707ae3
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2018-12-09 14:52:06 -0700

    re-add docs folder

---
+++ docs/precache-manifest.fc1d11b4c6e5380d0e34a22326af4b22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb6ad4691ba0012fb2e8",
+    "url": "/js/about.994ccd4d.js"
+  },
+  {
+    "revision": "91f31efbd0c43a872e58",
+    "url": "/css/app.f9bdd4af.css"
+  },
+  {
+    "revision": "91f31efbd0c43a872e58",
+    "url": "/js/app.b34449bc.js"
+  },
+  {
+    "revision": "a6ad9bcb7eff975f2d57",
+    "url": "/js/chunk-vendors.151264fd.js"
+  },
+  {
+    "revision": "b2504c1e0def38265a6d326422309674",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
