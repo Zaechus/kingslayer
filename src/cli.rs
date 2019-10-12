a674b845647d5be895652fe422af899ad2de5571
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2019-10-12 11:12:13 -0400

    small change

--- src/cli.rs
+++ src/cli.rs
@@ -169,7 +169,7 @@ Some available commands:
     // manages actions taken by Enemies in the current room
     fn combat(&self, world: &mut World) -> String {
         let mut events_str = String::new();
-        let mut loot: ItemMap = ItemMap::new();
+        let mut loot = ItemMap::new();
 
         for enemy in world.get_curr_room_mut().enemies_mut().values_mut() {
             if enemy.is_angry() && enemy.is_alive() {
